b2c33f527d15152f9482227faf240fcb3a94ab5c
miracl <williams.chigbu@miracl.com>
2021-07-20 21:42:29 +0100

    fix: menu/modal, resources visibility and favicon

--- content/homepage/resources.md
+++ content/homepage/resources.md
@@ -5,5 +5,6 @@ button: 'Github Project'
 buttonLink: 'https://github.com/camharris/volatility-oracle'
 ---
 ### Project submission to Eth.Global HackMoney Hackathon 
+
 For the [ETH Global Hack Money Hackathon](https://defi.ethglobal.com/) we created a Chainlink external adapter that powered requests for our Yield Farming Volatility Oracle. For specific information on our project submission see [here](https://showcase.ethglobal.co/hackmoney2021/yield-volatility-oracle)
 - [Link to github project](https://github.com/camharris/volatility-oracle)
--- themes/hugo-hero-theme/assets/js/scripts.js
+++ themes/hugo-hero-theme/assets/js/scripts.js
@@ -1,9 +1,23 @@
 var body = document.querySelector('body')
 var menuTrigger = document.querySelector('#toggle-main-menu-mobile');
 var menuContainer = document.querySelector('#main-menu-mobile');
+const header = document.querySelector('.header.header-absolute')
 
-menuTrigger.onclick = function() {
+function toggleModal() {
     menuContainer.classList.toggle('open');
     menuTrigger.classList.toggle('is-active')
     body.classList.toggle('lock-scroll')
+}
+
+menuTrigger.onclick = function() {
+    toggleModal()
+    header.classList.add('header-to-fixed')
+    let isMenuOpen = header.classList.contains('header-to-fixed')
+    let mobileMenuLinks = document.querySelectorAll('.mobLinks')
+    if (isMenuOpen) {
+        mobileMenuLinks.forEach( mobileMenuLink => {
+            mobileMenuLink.addEventListener('click', toggleModal )
+        })
+    }
+    
 }
